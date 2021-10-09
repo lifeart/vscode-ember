49695612a4a4d76a2dff9aa6377044c5cf50d5fe
Aleksandr Kanunnikov <lifeart92@gmail.com>
2021-10-10 01:30:59 +0300

    update configs

--- src/worker-extension.ts
+++ src/worker-extension.ts
@@ -1,19 +1,13 @@
 "use strict";
 
-import { COMMANDS as ELS_COMMANDS } from './constants';
 import { UsagesProvider } from './usages-provider';
 import {
   workspace,
   ExtensionContext,
-  StatusBarItem,
   window,
   commands,
-  languages,
-  InputBoxOptions,
-  StatusBarAlignment,
   Uri
 } from "vscode";
-import { isEmberCliProject, isGlimmerXProject } from './workspace-utils';
 import {
   Message,
   ErrorAction,
@@ -23,34 +17,9 @@ import {
   RevealOutputChannelOn,
   ExecuteCommandRequest
 } from "vscode-languageclient/browser";
-import { provideCodeLenses } from './lenses';
-let ExtStatusBarItem: StatusBarItem;
+import { COMMANDS as ELS_COMMANDS } from './constants';
+
 export async function activate(context: ExtensionContext) {
-  // The server is implemented in node
-  let config: any = {
-    codeLens: {
-      relatedFiles:  true
-    }
-  };
-  try {
-    config = workspace.getConfiguration("els");
-  } catch (e) {
-    // EOL
-  }
-  // The debug options for the server
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-
-  if (!(await isEmberCliProject())) {
-    if (!(await isGlimmerXProject())) {
-      return;
-    }
-  }
-
-  const syncExtensions = ["js", "ts", "hbs"];
-  if (config.codeLens.relatedFiles) {
-    syncExtensions.push("less", "scss", "css");
-  }
 
   // Options to control the language client
   let clientOptions: LanguageClientOptions = {
@@ -58,6 +27,7 @@ export async function activate(context: ExtensionContext) {
     documentSelector: ["handlebars", "javascript", "typescript"],
     outputChannelName: "Unstable Ember Language Server",
     revealOutputChannelOn: RevealOutputChannelOn.Never,
+    synchronize: {},
     initializationOptions: { editor: 'vscode' },
     errorHandler: {
       error(error: Error, message: Message | undefined, count: number): ErrorAction {
@@ -67,47 +37,8 @@ export async function activate(context: ExtensionContext) {
       closed() {
         return CloseAction.DoNotRestart;
       }
-    },
-    synchronize: {
-      fileEvents: workspace.createFileSystemWatcher(
-        `**/*.{${syncExtensions.join(",")}}`
-      )
     }
   };
-
-  ExtStatusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 10);
-  ExtStatusBarItem.text = "$(telescope) Ember Loading...";
-  ExtStatusBarItem.command = ELS_COMMANDS.SET_STATUS_BAR_TEXT;
-  ExtStatusBarItem.show();
-
-  // Push the disposable to the context's subscriptions so that the
-  // client can be deactivated on extension deactivation
-  context.subscriptions.push(
-    commands.registerCommand(ELS_COMMANDS.SET_STATUS_BAR_TEXT, async () => {
-      ExtStatusBarItem.text = "$(telescope) " + 'Reloading projects...';
-      await commands.executeCommand(ELS_COMMANDS.RELOAD_PROJECT);
-      ExtStatusBarItem.text = "$(telescope) " + 'Ember';
-    })
-  );
-
-
-  context.subscriptions.push(
-    commands.registerCommand(ELS_COMMANDS.PROXY_COMMAND, async (commandName: string, commandOpts: any, callbackCommandName: string, tail: any = {}) => {
-      try {
-        let result = await commands.executeCommand(commandName, ...commandOpts);
-        let document = workspace.rootPath;
-        if (!document && workspace.workspaceFolders && workspace.workspaceFolders.length) {
-          document = workspace.workspaceFolders[0].uri.fsPath;
-        }
-        if (window.activeTextEditor) {
-          document = window.activeTextEditor.document.uri.fsPath;
-        }
-        commands.executeCommand(callbackCommandName, document, result, tail);
-      } catch(e) {
-        window.showErrorMessage(e.toString());
-      }
-    })
-  );
 
   context.subscriptions.push(
     commands.registerCommand('els.fs.readFile', async (filePath: Uri) => {
@@ -144,64 +75,28 @@ export async function activate(context: ExtensionContext) {
     })
   )
 
-  context.subscriptions.push(
-    commands.registerCommand(ELS_COMMANDS.GET_USER_INPUT, async (opts: InputBoxOptions, callbackCommandName: string, tail: any) => {
-      try {
-        let what = await window.showInputBox(opts);
-        let document = workspace.rootPath;
-        if (!document && workspace.workspaceFolders && workspace.workspaceFolders.length) {
-          document = workspace.workspaceFolders[0].uri.fsPath;
-        }
-        if (window.activeTextEditor) {
-          document = window.activeTextEditor.document.uri.fsPath;
-        }
-        commands.executeCommand(callbackCommandName, document, what, tail);
-      } catch(e) {
-        window.showErrorMessage(e.toString());
-      }
-    })
-  );
 
-  context.subscriptions.push(
-    commands.registerCommand(ELS_COMMANDS.RUN_IN_EMBER_CLI, async () => {
-      let what = await window.showInputBox({
-        placeHolder: "Enter ember-cli command"
-      });
-      if (!what) {
-        return;
-      }
-      try {
-        let document = workspace.rootPath;
-        if (!document && workspace.workspaceFolders && workspace.workspaceFolders.length) {
-          document = workspace.workspaceFolders[0].uri.fsPath;
-        }
-        if (window.activeTextEditor) {
-          document = window.activeTextEditor.document.uri.fsPath;
-        }
-        commands.executeCommand(ELS_COMMANDS.EXECUTE_IN_EMBER_CLI, document, what);
-      } catch (e) {
-        window.showErrorMessage(e.toString());
-      }
-    })
-  );
+
   // Create the language client and start the client.
   let disposable = createWorkerLanguageClient(context, clientOptions);
 
   const fileUsagesProvider = new UsagesProvider();
 
-
   disposable.onReady().then(() => {
     disposable.onRequest(ExecuteCommandRequest.type.method, async ({command, arguments: args}) => {
       return commands.executeCommand(command, ...args);
     });
-    commands.executeCommand(ELS_COMMANDS.SET_CONFIG, config);
-    ExtStatusBarItem.text = "$(telescope) " + 'Ember';
+    commands.executeCommand(ELS_COMMANDS.SET_CONFIG, {local: {
+      collectTemplateTokens: false,
+      useBuiltinLinting: false
+    }});
 
     window.onDidChangeActiveTextEditor(()=>{
       if (window.activeTextEditor) {
         fileUsagesProvider.refresh();
       }
     });
+
     let treeView = window.createTreeView('els.fileUsages', {
       treeDataProvider: fileUsagesProvider
     });
@@ -210,38 +105,15 @@ export async function activate(context: ExtensionContext) {
   });
   context.subscriptions.push(disposable.start());
 
-  async function openRelatedFile(...rawFile) {
-    let url = Uri.file(rawFile.join(""));
-    commands.executeCommand("vscode.open", url);
-  }
-
-
-
-  if (config.codeLens.relatedFiles) {
-    const langs = [
-      "javascript",
-      "typescript",
-      "handlebars",
-      "css",
-      "less",
-      "scss"
-    ];
-    context.subscriptions.push(
-      commands.registerCommand(ELS_COMMANDS.OPEN_RELATED_FILE, openRelatedFile)
-    );
-    langs.forEach(language => {
-      context.subscriptions.push(
-        languages.registerCodeLensProvider(language, { provideCodeLenses })
-      );
-    });
-  }
-
 }
 
 function createWorkerLanguageClient(context: ExtensionContext, clientOptions: LanguageClientOptions) {
 	// Create a worker. The worker main file implements the language server.
-	const serverMain = Uri.joinPath(context.extensionUri, 'dist/web/server/browserServerMain.js');
-  const worker = new Worker(serverMain.toString());
+	const serverMain = Uri.joinPath(context.extensionUri, 'node_modules/@lifeart/ember-language-server/dist/bundled/start-worker-server.js');
+  const worker = new Worker(serverMain.toString(), {
+    name: '@lifeart/ember-language-server',
+    type: 'classic'
+  });
 	// create the language server client to communicate with the server running in the worker
 	return new LanguageClient('emberLanguageServer', "Unstable Ember Language Server", clientOptions, worker);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -52,66 +52,8 @@ const browserClientConfig = /** @type WebpackConfig */ {
 	performance: {
 		hints: false,
 	},
-	devtool: 'source-map',
+	// devtool: 'source-map',
 };
 
-const browserServerConfig = /** @type WebpackConfig */ {
-	context: path.join(__dirname, 'node_modules', '@lifeart/ember-language-server'),
-	mode: 'none',
-	target: 'webworker', // web extensions run in a webworker context
-	entry: {
-		browserServerMain: './src/start-worker-server.ts',
-	},
-	output: {
-		filename: '[name].js',
-		path: path.join(__dirname, 'dist',  'web', 'server'),
-		libraryTarget: 'var',
-		library: 'serverExportVar',
-	},
-	resolve: {
-		mainFields: ['module', 'main'],
-		extensions: ['.ts', '.js'], // support ts-files and js-files
-		alias: {},
-		fallback: {
-			path: require.resolve("path-browserify"),
-      util: false,
-      os: require.resolve("os-browserify/browser"),
-      fs: require.resolve("browserify-fs"),
-      tty: false,
-      assert: false,
-      debug: false,
-      net: false,
-      stream: false,
-      'is-typed-array': require.resolve('is-typed-array'),
-      'which-typed-array': require.resolve('which-typed-array'),
-      'is-generator-function': require.resolve('is-generator-function'),
-      'is-arguments': require.resolve('is-arguments'),
-      'available-typed-arrays': require.resolve('available-typed-arrays'),
-      'foreach': require.resolve('foreach'),
-      'array-filter': require.resolve('array-filter'),
-		},
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				exclude: /node_modules/,
-				use: [
-					{
-						loader: 'ts-loader',
-					},
-				],
-			},
-		],
-	},
-	externals: {
-		vscode: 'commonjs vscode', // ignored because it doesn't exist
-	},
-	performance: {
-		hints: false,
-	},
-	devtool: 'source-map',
-};
 
-// browserServerConfig
-module.exports = [browserClientConfig, browserServerConfig];
+module.exports = [browserClientConfig];
